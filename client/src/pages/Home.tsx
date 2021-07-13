a96df41747e3b6ce46430ac35281509531b254c3
assemaybm <assem4real@yahoo.com>
2021-07-13 12:24:41 +0200

    Question Box

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,12 +10,63 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Heading,
 } from "@chakra-ui/react";
 import { LoginFrom } from "../components/LoginFrom";
 import { RegisterFrom } from "../components/RegisterForm";
 
-interface HomeProps {}
+import { FiUser,  FiCalendar } from "react-icons/fi";
+interface QuestionFormProps {}
+const QuestionForm: React.FC<QuestionFormProps> = () => {
+  return (
+    <Box display="flex" flexDirection="column" p="8px" my="10px" shadow="base">
+      <form>
+        <FormControl isRequired>
+          <FormLabel color="green.400" fontWeight="bold">
+            {" "}
+            question
+          </FormLabel>
+          <Input type="text" name="question" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel color="green.400" fontWeight="bold">
+            specilization
+          </FormLabel>
+          <Input type="text" name="specilization" />
+        </FormControl>
+      </form>
+      <Button marginTop="12px" marginX="auto">
+        submit
+      </Button>
+    </Box>
+  );
+};
 
+interface QuestionBoxProps {}
+const QuestionBox: React.FC<QuestionBoxProps> = () => {
+  return (
+    <Box pos="relative" width="100%" p="1rem" my="5px" shadow="base">
+      <Heading as="h5" size="xs" color="gray.300" display="flex">
+        <FiUser size="15px" />
+        <Box ml="4px">username</Box>
+      </Heading>
+      <Heading as="h3" boxShadow="sm" size="md" my="5px" p="5px">
+        what is this question taking about?
+      </Heading>
+      <Heading
+        pos="absolute"
+        right="20px"
+        bottom="1px"
+        color="gray.200"
+        fontSize="12px"
+      >
+        12-2-2021
+      </Heading>
+    </Box>
+  );
+};
+
+interface HomeProps {}
 export const Home: React.FC<RouteComponentProps> = ({ history, location }) => {
   const [date, setDate] = useState("2018-07-22");
 
@@ -27,88 +78,35 @@ export const Home: React.FC<RouteComponentProps> = ({ history, location }) => {
           flexDirection="column"
           alignItems="center"
           flex="4"
-          bgColor="whiteAlpha.200"
           shadow="base"
           p="0.5rem"
         >
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
-          <Box width="100%" p="1rem" my="5px" shadow="base">
-            <h4>the question form section</h4>
-          </Box>
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
+          <QuestionBox />
         </Flex>
 
         <Flex
           flex="1"
           p="0.5rem"
-          bgColor="whiteAlpha.400"
           flexDirection="column"
           maxH="auto"
           marginX="8px"
         >
-          <Box p="8px" shadow="base" bgColor="whiteAlpha.600" >
+          <Box p="8px" shadow="base">
             <Box>the filtering and display btns</Box>
             <Box>the filtering and display btns</Box>
           </Box>
-          <Box
-            display="flex"
-            flexDirection="column"
-            p="8px"
-            my="10px"
-            shadow="base"
-            bgColor="whiteAlpha.600"
-          >
-            <form>
-              <FormControl isRequired>
-                <FormLabel>add a question</FormLabel>
-                <Input type="text" name="question" />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel>specilization</FormLabel>
-                <Input type="text" name="specilization" />
-              </FormControl>
-            </form>
-            <Button my="5px" marginX="auto">
-              add
-            </Button>
-          </Box>
+          <QuestionForm />
         </Flex>
       </Flex>
     </>
--- client/src/smallComps/Skeleton.tsx
+++ client/src/smallComps/Skeleton.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Box, Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 export const Skeleton: React.FC = ({ children }) => {
   return (
-    <Box padding="1.5rem" minHeight="100vh"  width="100%">
+    <Box padding="1.5rem" minHeight="100vh" width="100%">
       {children}
     </Box>
   );
